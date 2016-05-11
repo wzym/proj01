30e9d7693bd29dec35cb2f0357b9bfd4d46f42f9
wzym <wzym@yandex.ru>
2016-05-12 00:18:11 +0300

    Первый набросок поля.

--- start.js
+++ start.js
@@ -1,2 +1,17 @@
-var start;
-var finish;
+
+function createField(width, height) {
+    var field = document.getElementById('field');
+    for (var i = 0; i < width * height; i++) {
+        field.appendChild(createCell());
+    }
+}
+
+function createCell() {
+    var div = document.createElement('div');
+    div.className = 'cell';
+    return div;
+}
+
+window.onload = function () {
+    createField(10, 10);
+};
